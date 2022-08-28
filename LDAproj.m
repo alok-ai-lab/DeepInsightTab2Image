4d559086b53f79b101dfe62630e8e7e3df96fb3a
alok-ai-lab <71104218+alok-ai-lab@users.noreply.github.com>
2022-08-29 00:00:57 +0900

    Update LDAproj.m

--- LDAproj.m
+++ LDAproj.m
@@ -3,23 +3,22 @@ function Y = LDAproj(data,Labels)
 
 J = func_LDA(data,Labels,'element');
 t=size(J,1);
-%J=kmeans(data,3);
-%T = 20; %clusters
-N=300;
-if t<N
-    N=t;
-end
+%N=300;
+%if t<N
+%    N=t;
+%end
 
-range=1:3:N;
-L=1:length(range);
-clust=zeros(size(J,1),length(range));
-for j=L
-    clust(:,j)=kmeans(J,range(j),'MaxIter',1000);
-end
-eva=evalclusters(J,clust,'CalinskiHarabasz');    
+%range=1:3:N;
+%L=1:length(range);
+%clust=zeros(size(J,1),length(range));
+%for j=L
+%    clust(:,j)=kmeans(J,range(j),'MaxIter',1000);
+%end
+%eva=evalclusters(J,clust,'CalinskiHarabasz');    
 
 %eva = evalclusters(J,'kmeans','CalinskiHarabasz','KList',1:3:N);
-T = eva.OptimalK;
+%T = eva.OptimalK;
+T = 3;
 if t>T
     fprintf('\nt cluster for LDA %d\n',T)
     J = kmeans(data,T);
